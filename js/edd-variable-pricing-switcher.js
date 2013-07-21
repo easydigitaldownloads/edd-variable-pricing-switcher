6bbaa7845fcd9483e1a480570242d8d1ddabae06
Barry Kooij <git@barrykooij.nl>
2013-07-21 18:06:06 +0200

    Changed JS ID to class

--- js/edd-variable-pricing-switcher.js
+++ js/edd-variable-pricing-switcher.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-	$('select[name=edd-variable-pricing-switcher]').change(function() {
+	$('.edd-variable-pricing-switcher').change(function() {
 		$(this).closest('form').submit();
 	})
 });
